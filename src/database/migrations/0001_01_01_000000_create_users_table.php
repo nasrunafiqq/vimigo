eb93bc83c35cabb387ca125f127d6deec31795c3
nasrunafiqq <nasrunafiq15@gmail.com>
2024-07-10 10:30:12 +0800

    - separate by services - create user seeder - alter user migration - install api sanctum - update docker compose to microservice

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,26 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('fav_genre');
+            $table->string('interest');
+            $table->string('playlist_history');
+            $table->string('physical_address');
+            $table->string('phone_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
@@ -43,7 +33,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        
     }
 };
