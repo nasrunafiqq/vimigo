3a158249ed86315fc5f70d008b8a86f39a8beb5f
nasrunafiqq <nasrunafiq15@gmail.com>
2024-07-10 10:29:49 +0800

    - separate by services - create user seeder - alter user migration - install api sanctum - update docker compose to microservice

---
+++ src/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('user', [UserController::class, 'all_user']);
+
