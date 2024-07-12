e4aea8a9522743fb890246b84b1b1d7e8aed8288
nasrunafiqq <nasrunafiq15@gmail.com>
2024-07-12 16:25:38 +0800

    update api with comment

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,7 +5,14 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// to get all user profile data
 Route::get('user', [UserController::class, 'AllUser']);
+
+// to get user playlist data (fav_genre, history,playlist history )
 Route::get('user/playlist/{id}', [UserController::class, 'UserPlaylist']);
+
+// to get user billing data (name,email, physical address, no phone)
 Route::get('user/billing/{id}', [UserController::class, 'userBilling']);
+
+// for billing service to update user email
 Route::post('user/edit/{id}', [UserController::class, 'UpdateUser']);
